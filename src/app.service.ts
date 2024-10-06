be8a07638c1ead492277b8da68e68317b578f2eb
Meo <80504563+HiIamMeo@users.noreply.github.com>
2024-10-06 23:29:55 +1100

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Danial LE!';
+    return 'Hello Danial LE!!!';
   }
 }
